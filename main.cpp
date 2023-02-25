6d7a844abfbc48d4a04a046312ade20c4680ee79
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-02-25 14:35:43 +0100

    Wrote function to detect if ssh is installed

--- main.cpp
+++ main.cpp
@@ -7,14 +7,22 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    bool translated(false);
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "cssh-windows_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
+            translated = true;
             break;
+        }
+    }
+    if (!translated)
+    {
+        const QString baseName = "cssh-windows_en_EN";
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
         }
     }
     MainWindowCSSH w;
--- mainwindowcssh.cpp
+++ mainwindowcssh.cpp
@@ -1,5 +1,38 @@
 #include "mainwindowcssh.h"
 #include "ui_mainwindowcssh.h"
+#include <QDir>
+#include <QFile>
+#include <QString>
+#include <QMessageBox>
+
+#ifdef _WIN32
+bool sshDetected()
+{
+    const QString appName = "ssh.exe";
+    QStringList path = qEnvironmentVariable("path").split(';',Qt::SkipEmptyParts);
+    path << QDir::toNativeSeparators("C:/WINDOWS/System32/OpenSSH");
+    for (const QString& dir : path)
+    {
+        QDir directory(dir);
+        if (directory.entryList(QDir::Files|QDir::Executable).contains(appName))
+            return true;
+    }
+    return false;
+}
+#else
+bool sshDectected()
+{
+    const QString appName = "ssh";
+    QStringList path = qEnvironmentVariable("PATH").split(':',Qt::SkipEmptyParts);
+    for (const QString& dir : path)
+    {
+        QDir directory(dir);
+        if (directory.entryList(QDir::Files|QDir::Executable|QDir::CaseSensitive).contains(appName))
+            return true;
+    }
+    return false;
+}
+#endif
 
 MainWindowCSSH::MainWindowCSSH(QWidget *parent)
     : QMainWindow(parent)
