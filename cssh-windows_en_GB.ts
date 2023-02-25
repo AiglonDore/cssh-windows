5d61643f5adc5f9979db3005931585f154edbafb
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-02-25 13:00:38 +0100

    Added translation files

---
+++ cssh-windows_en_GB.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>MainWindowCSSH</name>
+    <message>
+        <location filename="mainwindowcssh.ui" line="14"/>
+        <source>Cluster Secure Shell</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
