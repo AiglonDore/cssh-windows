cfc60b8ad1dd98cd499f8fc60e8e107f09533d30
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-02-26 12:10:41 +0100

    Updated translations

--- cssh-windows_en_GB.ts
+++ cssh-windows_en_GB.ts
@@ -2,21 +2,146 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_GB">
 <context>
+    <name>ConfigFileWidget</name>
+    <message>
+        <location filename="configfilewidget.ui" line="14"/>
+        <source>Config file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="14"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="15"/>
+        <source>Save as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="16"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="17"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="36"/>
+        <location filename="configfilewidget.cpp" line="76"/>
+        <location filename="configfilewidget.cpp" line="93"/>
+        <source>Unable to open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="36"/>
+        <location filename="configfilewidget.cpp" line="76"/>
+        <location filename="configfilewidget.cpp" line="93"/>
+        <source>File at location &quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="36"/>
+        <location filename="configfilewidget.cpp" line="76"/>
+        <location filename="configfilewidget.cpp" line="93"/>
+        <source>&quot; cannot be opened. Aborting task.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="88"/>
+        <source>Save file as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="88"/>
+        <source>All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="108"/>
+        <location filename="configfilewidget.cpp" line="118"/>
+        <source>File not saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configfilewidget.cpp" line="108"/>
+        <location filename="configfilewidget.cpp" line="118"/>
+        <source>Modifications are occuring on the configuration file. Do you want to save them?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>LicenceDialog</name>
     <message>
         <location filename="licencedialog.ui" line="14"/>
-        <location filename="licencedialog.cpp" line="12"/>
+        <location filename="licencedialog.cpp" line="30"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="licencedialog.cpp" line="15"/>
+        <source>Export as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="20"/>
+        <source>File &quot;LICENSE.md&quot; not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="34"/>
         <source>Warranty license</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="licencedialog.cpp" line="18"/>
-        <source>Copy license</source>
+        <location filename="licencedialog.cpp" line="35"/>
+        <source># DISCLAIMER OF WARRANTY
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM &quot;AS IS&quot; WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.</source>
+        <comment>Part of the GPL3 license</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="45"/>
+        <source>Liability license</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="46"/>
+        <source># DISCLAIMER OF LIABILITY
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</source>
+        <comment>Part of the GPL3 license</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="57"/>
+        <source>Network information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="83"/>
+        <source>Export information as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="83"/>
+        <source>Text document (*.txt);;Markdown (*.md)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="88"/>
+        <source>Unable to write file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="88"/>
+        <source>File at location &quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="licencedialog.cpp" line="88"/>
+        <source>&quot; cannot be opened. Aborting export task.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -119,7 +244,7 @@
     </message>
     <message>
         <location filename="mainwindowcssh.ui" line="152"/>
-        <source>Copy</source>
+        <source>Liability</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -153,17 +278,39 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindowcssh.cpp" line="54"/>
+        <location filename="mainwindowcssh.cpp" line="59"/>
         <source>Tool bar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindowcssh.cpp" line="74"/>
+        <location filename="mainwindowcssh.cpp" line="69"/>
+        <location filename="mainwindowcssh.cpp" line="81"/>
+        <source>SSH not detected</source>
+        <comment>SSH is the name of a program</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="69"/>
+        <source>SSH executable cannot be found on PATH. This program is useless without it. You can install using the optional fetaures of Windows: it&apos;s the &quot;SSH-Client&quot; feature.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="81"/>
+        <source>SSH executable cannot be found on PATH. This program is useless without it. Here is your Linux version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="82"/>
+        <source>Check on Intenet which package you must install and how. Usually, it is named &quot;openssh-client&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="98"/>
         <source>No command to execute.</source>
         <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
-        <location filename="mainwindowcssh.cpp" line="80"/>
+        <location filename="mainwindowcssh.cpp" line="104"/>
         <source>Adding %n new host.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
@@ -171,18 +318,73 @@
         </translation>
     </message>
     <message>
-        <location filename="mainwindowcssh.cpp" line="84"/>
+        <location filename="mainwindowcssh.cpp" line="109"/>
         <source>Executing command.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindowcssh.cpp" line="90"/>
+        <location filename="mainwindowcssh.cpp" line="116"/>
         <source>Cluster SSH information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindowcssh.cpp" line="90"/>
+        <location filename="mainwindowcssh.cpp" line="116"/>
         <source>This is a tool to connect to a cluster of hosts, using SSH, on Linux and Windows hosts.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="152"/>
+        <source>Open SSH configuration file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="152"/>
+        <source>All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="160"/>
+        <source>Load script path...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="160"/>
+        <source>Shell scripts (*.sh,*.bash);;PowerShell scripts (*.ps1);;Windows Command Batch Script (*.bat);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="163"/>
+        <source>No script to be loaded.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="170"/>
+        <source>Unable to open script</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="170"/>
+        <source>File at location &quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="170"/>
+        <source>&quot; cannot be opened. Aborting task.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="175"/>
+        <source>Executing a non-executable file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="175"/>
+        <source>You are trying to execute a script that is not defined as executable. Please double check the file or change its permission. If you are sur of what you are doing, you can proceed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindowcssh.cpp" line="178"/>
+        <source>Aborting task.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
